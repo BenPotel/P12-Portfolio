bdcdffe8a8995dc845763ca5520414f02b398302
BenPotel <benjamin.potel@proton.me>
2024-05-21 19:04:55 +0200

    Portfolio update, accessibility tweaks

--- script/script.js
+++ script/script.js
@@ -110,7 +110,22 @@ function createPortfolioFromJSON() {
       });
     });
 }
+document.addEventListener("DOMContentLoaded", function () {
+  var backToTopButton = document.getElementById("back-to-top");
 
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 300) {
+      backToTopButton.style.display = "block";
+    } else {
+      backToTopButton.style.display = "none";
+    }
+  });
+
+  backToTopButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+});
 // Call the functions to execute the code
 handleNavbarScroll();
 handleNavbarCollapse();
